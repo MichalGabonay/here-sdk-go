638f2fb266ca6a875885cab95a7453c4e64f055d
Michal Gabonay <michal.gabonay@gmail.com>
2019-09-18 10:44:52 +0200

    changed client

--- here.go
+++ here.go
@@ -2,13 +2,14 @@ package heremaps
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 // HereClient defines HERE map client
 type HereClient struct {
-	appID   string
-	appCode string
+	AppID   string
+	AppCode string
 }
 
 // Address represent address returned from HERE maps API
@@ -37,27 +38,32 @@ type RouteSummary struct {
 // NewClient returns a new Medium API client which can be used to make RPC requests.
 func NewClient(id, code string) *HereClient {
 	return &HereClient{
-		appID:   id,
-		appCode: code,
+		AppID:   id,
+		AppCode: code,
 	}
 }
 
 // SearchLocation - returns structured address and coordinates for location from address written as string
 func (hc *HereClient) SearchLocation(input string) (*GeocodeOutput, error) {
-	url := buildURLForGeocodeRequest(hc.appID, hc.appCode, input)
+	url := buildURLForGeocodeRequest(hc.AppID, hc.AppCode, input)
+
+	log.Print("url: " + url)
 
 	response, err := http.Get(url)
 	if err != nil {
+		log.Print("err1")
 		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
+		log.Print("err2")
 		return nil, err
 	}
 
 	geocode, err := parseGeocode(body)
 	if err != nil {
+		log.Print("err3")
 		return nil, err
 	}
 
@@ -65,7 +71,7 @@ func (hc *HereClient) SearchLocation(input string) (*GeocodeOutput, error) {
 }
 
 func (hc *HereClient) CalculateRoute(routingInput *RoutingInput) (*RoutingOutput, error) {
-	url := buildURLForRoutingRequest(hc.appID, hc.appCode, routingInput)
+	url := buildURLForRoutingRequest(hc.AppID, hc.AppCode, routingInput)
 
 	response, err := http.Get(url)
 	if err != nil {
